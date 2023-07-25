ad64c9fdd349a25e6dd0d7ebe589522c714baa97
SumamRaj <140527257+SumamRaj@users.noreply.github.com>
2023-07-25 12:33:45 -0400

    Update streamlit_app.py
    Change the error in header text and add Emojis to the menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New healthy Diner')
-streamlit.header('Breakfast Favorites🥣 🥗 🐔 🥑🍞')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗Omega 3 and Blueberry oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
